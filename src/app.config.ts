1abe55040f35eb8f134094c36dba9871fb88df49
luo <1156909789@qq.com>
2021-02-24 08:56:23 +0800

    init

---
+++ src/app.config.ts
@@ -0,0 +1,32 @@
+export default {
+  pages: [
+    'pages/home/index',
+    'pages/index/index',
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black'
+  },
+  tabBar: {
+    color: '#bfbfbf',
+    selectedColor: '#d81e06',
+    backgroundColor: '#fff',
+    borderStyle: '#F7F7F7',
+    list: [
+      {
+        pagePath: 'pages/home/index',
+        text: '自选',
+        iconPath: 'assets/tabbar/home.png',
+        selectedIconPath: 'assets/tabbar/home-a.png'
+      },
+      {
+        pagePath: 'pages/index/index',
+        text: '推送',
+        iconPath: 'assets/tabbar/push.png',
+        selectedIconPath: 'assets/tabbar/push-a.png'
+      }
+    ]
+  }
+}
