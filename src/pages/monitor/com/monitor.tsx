b05ca109bac054cf036ac4cceb318757700f00d5
luo <1156909789@qq.com>
2021-03-09 12:57:56 +0800

    feat:截图上传

---
+++ src/pages/monitor/com/monitor.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {View} from '@tarojs/components';
+
+export function Monitor() {
+    return <View></View>
+}
---
+++ src/pages/monitor/index.tsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {Monitor} from '@/pages/monitor/com/monitor';
+
+class Index extends Component {
+    render() {
+        return <Monitor />;
+    }
+}
+
+export default Index
